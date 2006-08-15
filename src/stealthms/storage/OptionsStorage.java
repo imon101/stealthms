3f5ffe303f24e597d2d98684bd62868649faa8bf
rysenko <rysenko@e7f1ab8d-be1a-0410-a091-7549dcdff8bd>
2006-08-15 17:46:59 +0000

    First step of OptionsStorage redesign

--- src/stealthms/storage/OptionsStorage.java
+++ src/stealthms/storage/OptionsStorage.java
@@ -1,33 +1,15 @@
 package stealthms.storage;
 
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
+import java.io.*;
+import java.util.Enumeration;
+import java.util.Hashtable;
 
-import javax.microedition.rms.RecordStore;
-import javax.microedition.rms.RecordStoreException;
+import javax.microedition.rms.*;
 
 public class OptionsStorage {
-	private static String User;
-
-	private static String Url;
-
-	private static String SMUser;
-
-	private static String SMPass;
-
-	private static String Gates;
-
-	private static String Copy;
+	private static Hashtable options;
 	
 	private static String[] Titles;
-	
-	private static int LastTitle;
-	
-	private static String Family;
-
-	private static int TranslitStat;
 	
 	public static void saveSettings() {
 		RecordStore recordStore;
@@ -39,18 +21,16 @@ public class OptionsStorage {
 		ByteArrayOutputStream baos = new ByteArrayOutputStream();
 		DataOutputStream outputStream = new DataOutputStream(baos);
 		try {
-			outputStream.writeUTF(User);
-			outputStream.writeUTF(Url);
-			outputStream.writeUTF(SMUser);
-			outputStream.writeUTF(SMPass);
-			outputStream.writeUTF(Gates);
-			outputStream.writeUTF(Copy);
-			outputStream.writeInt(LastTitle);
+			outputStream.writeInt(options.size());
+			for (Enumeration hashKeys = options.keys(); hashKeys.hasMoreElements();) {
+				String currentKey = (String)hashKeys.nextElement();
+				String currentValue = (String)options.get(currentKey);
+				outputStream.writeUTF(currentKey);
+				outputStream.writeUTF(currentValue);
+			}
 			for (int i = 0; i < 15; i++) {
 				outputStream.writeUTF(Titles[i]);
 			}
-			outputStream.writeUTF(Family);
-			outputStream.writeInt(TranslitStat);
 			byte[] bytes = baos.toByteArray();
 			if (recordStore.getNumRecords() == 0)
 				recordStore.addRecord(bytes, 0, bytes.length);
@@ -58,28 +38,28 @@ public class OptionsStorage {
 				recordStore.setRecord(1, bytes, 0, bytes.length);
 			outputStream.close();
 			recordStore.closeRecordStore();
-		} catch (Exception e) {
-		}
+		} catch (Exception e) {}
 	}
 
 	public static void loadSettings() {
 		RecordStore recordStore;
+		options = new Hashtable();
 		Titles = new String[15];
 		try {
 			recordStore = RecordStore.openRecordStore("SMS", false);
 		} catch (RecordStoreException rse) {
-			User = "somebody@cmc.ru";
-			Url = "smtp.umc.ua";
-			SMUser = "";
-			SMPass = "";
-			Gates = "8050:38050%@sms.umc.com.ua;8095:38095%@sms.umc.com.ua;8066:38066%@sms.jeans.com.ua;8097:38097%@sms.kyivstar.net;8067:38067%@sms.kyivstar.net;8096:38096%@sms.kyivstar.net;8068:38068%@sms.mobi.ua";
-			Copy = "";
-			LastTitle = -1;
+			setUser("somebody@cmc.ru");
+			setUrl("smtp.umc.ua");
+			setSMUser("");
+			setSMPass("");
+			setGates("8050:38050%@sms.umc.com.ua;8095:38095%@sms.umc.com.ua;8066:38066%@sms.jeans.com.ua;8097:38097%@sms.kyivstar.net;8067:38067%@sms.kyivstar.net;8096:38096%@sms.kyivstar.net;8068:38068%@sms.mobi.ua");
+			setCopy("");
+			setLastTitle(-1);
+			setFamily("8063%");
+			setTranslitStat(0);
 			for (int i = 0; i < 15; i++) {
 				Titles[i] = "";
 			}
-			Family = "8063%";
-			TranslitStat = 0;
 			saveSettings();
 			return;
 		}
@@ -87,80 +67,92 @@ public class OptionsStorage {
 			byte[] bytes = recordStore.getRecord(1);
 			ByteArrayInputStream bais = new ByteArrayInputStream(bytes);
 			DataInputStream inputStream = new DataInputStream(bais);
-			User = inputStream.readUTF();
-			Url = inputStream.readUTF();
-			SMUser = inputStream.readUTF();
-			SMPass = inputStream.readUTF();
-			Gates = inputStream.readUTF();
-			Copy = inputStream.readUTF();
-			LastTitle = inputStream.readInt();
+			int hashSize = inputStream.readInt();
+			for (int i = 0; i < hashSize; i++) {
+				String key = inputStream.readUTF();
+				String value = inputStream.readUTF();
+				options.put(key, value);
+			}
 			for (int i = 0; i < 15; i++) {
 				Titles[i] = inputStream.readUTF();
 			}
-			Family = inputStream.readUTF();
-			TranslitStat = inputStream.readInt();			
 			inputStream.close();
 			recordStore.closeRecordStore();
-		} catch (Exception e) {
-		}
+		} catch (Exception e) {}
 	}
 
 	public static String getCopy() {
-		return Copy;
+		return (String)options.get("Copy");
 	}
 
 	public static void setCopy(String copy) {
-		Copy = copy;
+		options.put("Copy", copy);
 	}
 
 	public static String getGates() {
-		return Gates;
+		return (String)options.get("Gates");
 	}
 
 	public static void setGates(String gates) {
-		Gates = gates;
+		options.put("Gates", gates);
 	}
 
 	public static String getSMPass() {
-		return SMPass;
+		return (String)options.get("SMPass");
 	}
 
 	public static void setSMPass(String pass) {
-		SMPass = pass;
+		options.put("SMPass", pass);
 	}
 
 	public static String getSMUser() {
-		return SMUser;
+		return (String)options.get("SMUser");
 	}
 
 	public static void setSMUser(String user) {
-		SMUser = user;
+		options.put("SMUser", user);
 	}
 
 	public static String getUrl() {
-		return Url;
+		return (String)options.get("Url");
 	}
 
 	public static void setUrl(String url) {
-		Url = url;
+		options.put("Url", url);
 	}
 
 	public static String getUser() {
-		return User;
+		return (String)options.get("User");
 	}
 
 	public static void setUser(String user) {
-		User = user;
+		options.put("User", user);
 	}
 
 	public static int getLastTitle() {
-		return LastTitle;
+		return Integer.parseInt((String)options.get("LastTitle"));
 	}
 
 	public static void setLastTitle(int lastTitle) {
-		LastTitle = lastTitle;
+		options.put("LastTitle", Integer.toString(lastTitle));
 	}
 
+	public static String getFamily() {
+		return (String)options.get("Family");
+	}
+
+	public static void setFamily(String family) {
+		options.put("Family", family);
+	}
+
+	public static int getTranslitStat() {
+		return Integer.parseInt((String)options.get("TranslitStat"));
+	}
+
+	public static void setTranslitStat(int translitstat) {
+		options.put("TranslitStat", Integer.toString(translitstat));
+	}
+	
 	public static String[] getTitles() {
 		return Titles;
 	}
@@ -168,20 +160,4 @@ public class OptionsStorage {
 	public static void setTitles(String[] titles) {
 		Titles = titles;
 	}
-	
-	public static String getFamily() {
-		return Family;
-	}
-
-	public static void setFamily(String family) {
-		Family = family;
-	}
-
-	public static int getTranslitStat() {
-		return TranslitStat;
-	}
-
-	public static void setTranslitStat(int translitstat) {
-		TranslitStat = translitstat;
-	}		
 }
