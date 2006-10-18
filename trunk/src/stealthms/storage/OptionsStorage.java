b085feab18a037d2b19bd78e901dd7287d32f144
rysenko <rysenko@e7f1ab8d-be1a-0410-a091-7549dcdff8bd>
2006-10-18 22:21:36 +0000

    Merging E2S

--- trunk/src/stealthms/storage/OptionsStorage.java
+++ trunk/src/stealthms/storage/OptionsStorage.java
@@ -40,7 +40,7 @@ public class OptionsStorage {
 			recordStore.closeRecordStore();
 		} catch (Exception e) {}
 	}
-
+	
 	public static void loadSettings() {
 		RecordStore recordStore;
 		options = new Hashtable();
@@ -59,6 +59,9 @@ public class OptionsStorage {
 			setHttp("8063%");
 			setTranslitStat(1);
 			setAuthLogin(0);
+			setE2S("");
+			setE2SUser("");
+			setE2SPass("");
 			for (int i = 0; i < 15; i++) {
 				Titles[i] = "";
 			}
@@ -82,67 +85,67 @@ public class OptionsStorage {
 			recordStore.closeRecordStore();
 		} catch (Exception e) {}
 	}
-
+	
 	public static String getCopy() {
 		return (String)options.get("Copy");
 	}
-
+	
 	public static void setCopy(String copy) {
 		options.put("Copy", copy);
 	}
-
+	
 	public static String getGates() {
 		return (String)options.get("Gates");
 	}
-
+	
 	public static void setGates(String gates) {
 		options.put("Gates", gates);
 	}
-
+	
 	public static String getSMPass() {
 		return (String)options.get("SMPass");
 	}
-
+	
 	public static void setSMPass(String pass) {
 		options.put("SMPass", pass);
 	}
-
+	
 	public static String getSMUser() {
 		return (String)options.get("SMUser");
 	}
-
+	
 	public static void setSMUser(String user) {
 		options.put("SMUser", user);
 	}
-
+	
 	public static String getUrl() {
 		return (String)options.get("Url");
 	}
-
+	
 	public static void setUrl(String url) {
 		options.put("Url", url);
 	}
-
+	
 	public static String getUser() {
 		return (String)options.get("User");
 	}
-
+	
 	public static void setUser(String user) {
 		options.put("User", user);
 	}
-
+	
 	public static int getLastTitle() {
 		return Integer.parseInt((String)options.get("LastTitle"));
 	}
-
+	
 	public static void setLastTitle(int lastTitle) {
 		options.put("LastTitle", Integer.toString(lastTitle));
 	}
-
+	
 	public static String getFamily() {
 		return (String)options.get("Family");
 	}
-
+	
 	public static void setFamily(String family) {
 		options.put("Family", family);
 	}
@@ -150,15 +153,15 @@ public class OptionsStorage {
 	public static String getHttp() {
 		return (String)options.get("HTTP");
 	}
-
+	
 	public static void setHttp(String family) {
 		options.put("HTTP", family);
 	}
-
+	
 	public static int getTranslitStat() {
 		return Integer.parseInt((String)options.get("TranslitStat"));
 	}
-
+	
 	public static void setTranslitStat(int translitstat) {
 		options.put("TranslitStat", Integer.toString(translitstat));
 	}
@@ -166,7 +169,7 @@ public class OptionsStorage {
 	public static int getAuthLogin() {
 		return Integer.parseInt((String)options.get("AuthLogin"));
 	}
-
+	
 	public static void setAuthLogin(int authlogin) {
 		options.put("AuthLogin", Integer.toString(authlogin));
 	}
@@ -174,8 +177,31 @@ public class OptionsStorage {
 	public static String[] getTitles() {
 		return Titles;
 	}
-
+	
 	public static void setTitles(String[] titles) {
 		Titles = titles;
+	}
+	
+	public static String getE2SUser() {
+		return (String)options.get("E2SUser");
+	}
+	
+	public static void setE2SUser(String E2SUser) {
+		options.put("E2SUser", E2SUser);
+	}
+	
+	public static String getE2SPass() {
+		return (String)options.get("E2SPass");
+	}
+	public static void setE2SPass(String E2SPass) {
+		options.put("E2SPass", E2SPass);
+	}
+	
+	public static String getE2S() {
+		return (String)options.get("E2S");
+	}
+	
+	public static void setE2S(String mask) {
+		options.put("E2S", mask);
 	}
 }
